6b0fb3ebb3583e0718053412d98bb019641b504a
ahorvath <arpad.horvath@hs-osnabrueck.de>
2024-10-22 16:48:34 +0200

    score and hit detection for circless

--- Assets/PenConnection.cs
+++ Assets/PenConnection.cs
@@ -62,22 +62,6 @@ public class PenConnection : MonoBehaviour
         udpClient = new UdpClient(8081);
         remoteEndPoint = new IPEndPoint(IPAddress.Any, 8081);
     }
-    // Vector3 ToUnityCoordinates(int x_osu, int y_osu)
-    // {
-    //     // Step 1: Normalize the osu coordinates (0 to 1 range)
-    //     float x_norm = x_osu / 512f;
-    //     float y_norm = y_osu / 384f;
-    //
-    //     // Step 2: Calculate Unity's camera dimensions
-    //     float cameraHeight = 2f * gameCamera.orthographicSize;
-    //     float cameraWidth = cameraHeight * gameCamera.aspect;
-    //
-    //     // Step 3: Map normalized osu coordinates to Unity's world coordinates
-    //     float x_unity = (x_norm - 0.5f) * cameraWidth;
-    //     float y_unity = (y_norm - 0.5f) * cameraHeight;
-    //
-    //     return new Vector3(x_unity, y_unity, 0f); // Z is zero for 2D
-    // }
     void Update()
     {
         if (udpClient.Available > 0)
@@ -90,7 +74,7 @@ public class PenConnection : MonoBehaviour
             String[] stdfs = (message.Split(";"));
             float x = float.Parse(stdfs[1], en_us.NumberFormat);
             float y = float.Parse(stdfs[0], en_us.NumberFormat)-5;
-            Debug.Log("x: " +x + "y: " + y);
+           // Debug.Log("x: " +x + "y: " + y);
             x = Remap(x, 0, 83, -8.4f, 9.4f);
             y = Remap(y, 0, 118, -4.5f, 5.5f);
             Vector3 pos = new Vector3(x, y, 0.0f);
@@ -99,7 +83,6 @@ public class PenConnection : MonoBehaviour
             //pos.x = x;
             //pos.y = y;
             cursorDot.transform.localPosition = pos;
-            cursorDot.transform.rotation = Quaternion.Euler(0, 0, 0);
             // Debug.Log(pos);
             // Instantiate(cursorDot, pos, Quaternion.identity);
         }
